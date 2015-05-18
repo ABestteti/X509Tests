6f1724597183a967994ccf224cda40261f146c16
anderson <anderson.bestteti@gmail.com>
2015-05-18 00:05:26 -0300

    Quarto commit. Inclusao de mais um link com exemplo completo de assinatura de um arquivo XML: http://www.xinotes.net/notes/note/751/

--- X509Tests/src/com/acaosistemas/signing/X509Signinger.java
+++ X509Tests/src/com/acaosistemas/signing/X509Signinger.java
@@ -10,6 +10,7 @@ import javax.xml.crypto.dsig.XMLSignatureFactory;
  * @version 1.0
  * @see http://www.oracle.com/technetwork/articles/javase/dig-signature-api-140772.html
  * @see http://www.java-tips.org/java-ee-tips/xml-digital-signature-api/using-the-java-xml-digital-signatur-2.html
+ * @see http://www.xinotes.net/notes/note/751/
  * Teste de assinatura de um arquivo XML com um certificado padrao X.509.
  * Criado repositorio no GITHub.
  * 
