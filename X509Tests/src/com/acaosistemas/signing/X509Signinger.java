23312d611e78bceeab3beb59636136d5b0667082
anderson <anderson.bestteti@gmail.com>
2015-05-17 23:52:40 -0300

    Inclusao dos links de referencia.

--- X509Tests/src/com/acaosistemas/signing/X509Signinger.java
+++ X509Tests/src/com/acaosistemas/signing/X509Signinger.java
@@ -8,11 +8,11 @@ import javax.xml.crypto.dsig.XMLSignatureFactory;
 /**
  * @author Anderson Bestetti
  * @version 1.0
- * 
+ * @see http://www.oracle.com/technetwork/articles/javase/dig-signature-api-140772.html
+ * @see http://www.java-tips.org/java-ee-tips/xml-digital-signature-api/using-the-java-xml-digital-signatur-2.html
  * Teste de assinatura de um arquivo XML com um certificado padrao X.509.
  * Criado repositorio no GITHub.
- * 01
- * 02
+ * 
  * 
  */
 public class X509Signinger {
