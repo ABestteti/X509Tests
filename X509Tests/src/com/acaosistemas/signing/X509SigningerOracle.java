f1b0c36bec876c65f4cc9b7af8a73aa06307b2da
anderson.bestteti@gmail.com <anderson.bestteti@gmail.com>
2015-06-02 18:07:52 -0300

    Criacao da classe de testes com base no tutorial da Oracle.

---
+++ X509Tests/src/com/acaosistemas/signing/X509SigningerOracle.java
@@ -0,0 +1,18 @@
+/**
+ * @author Anderson Bestetti
+ * @version 1.0
+ * @see http://www.oracle.com/technetwork/articles/javase/dig-signature-api-140772.html
+ * @see http://www.java-tips.org/java-ee-tips/xml-digital-signature-api/using-the-java-xml-digital-signatur-2.html
+ * @see http://www.xinotes.net/notes/note/751/
+ * Teste de assinatura de um arquivo XML com um certificado padrao X.509.
+ **/
+package com.acaosistemas.signing;
+
+public class X509SigningerOracle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
